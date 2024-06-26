ddef4f8e5d0043f4092c3b430a6d1f1551dc1ab2
Inji Zakariyayeva <incizekeriyeyeva@gmail.com>
2024-06-26 22:57:31 +0400

    first-project-with-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "bulma/css/bulma.css";
+import "./App.css";
+import Course from "./Course";
+import Angular from "./images/bannerimg1.jpg";
+import Bootstrap from "./images/bannerimg2.jpg";
+import KompleWeb from "./images/bannerimg1.jpg";
+import Frontend from "./images/bannerimg2.jpg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="hero is-link">
+        <div className="hero-body">
+          <p className="title">My cources</p>
+        </div>
+      </section>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column">
+              <Course
+                image={Angular}
+                title="Angular"
+                description=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam maxime
+      omnis voluptatem, obcaecati assumenda esse ipsa provident sit quaerat
+      autem."
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={Bootstrap}
+                title="Bootstrap 5"
+                description=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam maxime
+      omnis voluptatem, obcaecati assumenda esse ipsa provident sit quaerat
+      autem."
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={KompleWeb}
+                title="Komple Web"
+                description=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam maxime
+      omnis voluptatem, obcaecati assumenda esse ipsa provident sit quaerat
+      autem."
+              />
+            </div>
+            <div className="column">
+              <Course
+                image={Frontend}
+                title="Frontend"
+                description=" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam maxime
+      omnis voluptatem, obcaecati assumenda esse ipsa provident sit quaerat
+      autem."
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
